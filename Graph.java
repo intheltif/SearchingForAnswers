e597b4617548bd64246913fba8e8b76af5d22ce9
Evert <weball1@catamount.wcu.edu>
2019-10-14 21:07:29 -0400

    Added pieces to reading input file

--- Graph.java
+++ Graph.java
@@ -1,4 +1,5 @@
-// Imports here
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Represents a Graph made of Vertices.
@@ -9,14 +10,54 @@
  */
 public class Graph {
 
-    public Graph() {
+    private static final int ADJ_SIZE = 2;
+    
+    /** 
+     * List of vertices in the graph where the index corresponds to the 
+     * vertex number.
+     */
+    private ArrayList<Vertex> vertexList;
 
-        // Construct
+    /** List at each vertex corresponds to the vertices adjacent to vertex */
+    private ArrayList<ArrayList<Vertex>> adjList;
+
+    /** Value of true represents an edge between two vertices */
+    private boolean [][] adjMatrix;
+
+    public Graph(File graphFile) {
+        
+        // Create two new empty ArrayLists of default capacity of ten elements.
+        this.vertexList = new ArrayList<Vertex>();
+        this.adjList    = new ArrayList<ArrayList<Vertex>>();
+        
+        try {
+            Scanner in = new Scanner(graphFile);
+
+            while(in.hasNextLine()) {
+
+                int from = in.nextInt();
+                int to   = in.nextInt();
+                // TODO Figure out a better way to do this
+                ArrayList<Vertex> pair = new ArrayList<>(ADJ_SIZE);
+                pair.add(0, from);
+                pair.add(1, to);
+
+                this.vertexList.add(new Vertex(from));
+                this.adjList
+
+            } // end while loop
+
+        } catch(FileNotFoundException fnfe) {
+            System.err.println("File not found. Enter an existing file.");
+            System.exit(1);
+        } catch(InputMismatchException ime) {
+            System.err.println("Vertices must be integers. Exiting..."
+            System.exit(2);
 
     } // end Graph constructor
     
     /**
-     * Performs the operations described in the handout
+     * Performs the operations described in the handout.
      */
     public void go() {
 
@@ -34,7 +75,7 @@ public class Graph {
      * @throws IllegalArgumentException if the source and destination vertices
      *         are not found in the graph.
      */
-    public findSourceDest() thorws IllegalArgumentException {
+    public findSourceDest() throws IllegalArgumentException {
 
     } // end findSourceDest method
 
--- Vertex.java
+++ Vertex.java
@@ -1,5 +1,3 @@
-// Imports here
-
 /**
  * This class represents a Vertex in a Graph. Each vertex has a unique ID
  * number and a color that represents its current state (visited, unvisited,
@@ -10,6 +8,8 @@
  */
 public class Vertex {
 
+    private static final String DEFAULT_COLOR = "white";
+
     /**
      * Unique integer corresponding to a vertex's id number. Vertices are
      * numbered from 0 to the number of vertices - 1
@@ -18,12 +18,65 @@ public class Vertex {
 
     /** Color/Classification of the vertex */ //TODO Maybe make an enum?
     private String color;
+    
+    /**
+     * Creates a Vertex based on a specified id and the default color. The
+     * default color is white (unvisited).
+     *
+     * @param id The unique integer representing the vertex's id number.
+     * @param color The color/classification of the vertex.
+     */
+    public Vertex(int id) {
+        
+        this.id = id;
+        this.color = DEFAULT_COLOR;
 
+    } // end Vertex constructor
+    
+    /**
+     * Creates a Vertex based on a specified id and color.
+     *
+     * @param id The unique integer representing the vertex's id number.
+     * @param color The color/classification of the vertex.
+     */
     public Vertex(int id, String color) {
         
         this.id = id;
         this.color = color;
 
     } // end Vertex constructor
+    
+    /**
+     * Changes the color of the vertex.
+     *
+     * @param color The new color of the vertex.
+     */
+    public void setColor(String color) {
+
+        this.color = color.toLowerCase();
+
+    } // end setColor method
+
+    /**
+     * Returns the color of a vertex.
+     *
+     * @return A String representing the color of a vertex.
+     */
+    public String getColor() {
+
+        return this.color;
+
+    } // end getColor method
+
+    /**
+     * Returns the unique id of a vertex.
+     *
+     * @return The unique id of a vertex.
+     */
+    public int getId() {
+
+        return this.id;
+
+    } // end getId method
 
 } // end Vertex class
